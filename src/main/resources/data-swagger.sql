d1f174386a55ab7dc29dc80fba4b9af0500b29f6
Sammir Alejandro Bolanos Luna <sammir.bolanos@mvm.com.co>
2026-04-19 10:29:24 -0500

    optimización

--- src/main/resources/data-swagger.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO qpr (nombre, estado, fecha) VALUES ('QPR-Login', 'ABIERTO', DATE '2026-04-10');
-INSERT INTO qpr (nombre, estado, fecha) VALUES ('QPR-Notificaciones', 'EN_PROGRESO', DATE '2026-04-12');
-INSERT INTO qpr (nombre, estado, fecha) VALUES ('QPR-Integracion-API', 'CERRADO', DATE '2026-04-15');
-INSERT INTO qpr (nombre, estado, fecha) VALUES ('QPR-Seguridad', 'ABIERTO', DATE '2026-04-18');
-INSERT INTO qpr (nombre, estado, fecha) VALUES ('QPR-Reportes', 'BLOQUEADO', DATE '2026-04-19');
